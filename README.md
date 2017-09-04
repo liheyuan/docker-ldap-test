94c382f038db9c62d24acf7c5034fec9b0ea9cac
coder4 <lihy@fenbi.com>
2017-09-04 17:23:11 +0800

    MOD: Add idlf file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # docker-ldap-test
+admin:cn=admin,dc=coder4,dc=com
+secret:admin
+
+ldapadd -h 192.168.99.100 -p 389 -w admin -D "cn=admin,dc=coder4,dc=com" -f ./add_test.ldif
--- run_ldap_docker.sh
+++ run_ldap_docker.sh
@@ -6,4 +6,4 @@ VOLUME_CONFIG='/var/lib/boot2docker/docker_data/ldap/config'
 eval $(docker-machine env $NODE_NAME)
 last_id=$(docker ps -l -q)
 docker rm -f $last_id
-docker run --volume "$VOLUME_DATA":/var/lib/ldap --volume "$VOLUME_CONFIG":/etc/ldap/slapd.d -p389:389 -p636:636 --env LDAP_TLS=false --env LDAP_ORGANISATION="Coder4 Company" --env LDAP_DOMAIN="coder4.com" --env LDAP_ADMIN_PASSWORD="hehehe" --detach osixia/openldap:1.1.9
+docker run --volume "$VOLUME_DATA":/var/lib/ldap --volume "$VOLUME_CONFIG":/etc/ldap/slapd.d -p389:389 -p636:636 --env LDAP_TLS=false --env LDAP_DOMAIN=coder4.com --detach osixia/openldap:1.1.9
